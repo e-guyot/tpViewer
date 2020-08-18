a030a8a9f53d81693631afb3f4cd2af66cb1ce0d
BuildTools <unconfigured@null.spigotmc.org>
2020-08-18 09:34:06 +0200

    HomeController return user in view

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -15,9 +15,10 @@ class HomeController extends AbstractController
      */
     public function index()
     {
+        $user = $this->getUser();
 
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'user' => $user,
         ]);
     }
 }
