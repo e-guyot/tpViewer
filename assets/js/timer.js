2c918a7ffd4f8fb177c0a666d1b61d895d06c0ee
e-guyot <guyot.estellee@gmail.com>
2020-08-20 14:34:20 +0200

    add timer.js

--- assets/js/timer.js
+++ assets/js/timer.js
@@ -1,58 +1,62 @@
-var startTime = 0
-var start = 0
-var end = 0
-var diff = 0
-var timerID = 0
-window.onload = chronoStart;
-function chrono(){
-	end = new Date()
-	diff = end - start
-	diff = new Date(diff)
-	var msec = diff.getMilliseconds()
-	var sec = diff.getSeconds()
-	var min = diff.getMinutes()
-	var hr = diff.getHours()-1
-	if (min < 10){
-		min = "0" + min
-	}
-	if (sec < 10){
-		sec = "0" + sec
-	}
-	if(msec < 10){
-		msec = "00" +msec
-	}
-	else if(msec < 100){
-		msec = "0" +msec
-	}
-	document.getElementById("chronotime").value = hr + ":" + min + ":" + sec + ":" + msec
-	timerID = setTimeout("chrono()", 10)
+var setTm=0;
+var tmStart=0;
+var tmNow=0;
+var tmInterv=0;
+var tTime=[]; //tableau des temps intermédiaires (lap)
+var nTime=0; //compteur des temps intermédiaires
+function affTime(tm){ //affichage du compteur
+   var vMin=tm.getMinutes();
+   var vSec=tm.getSeconds();
+   var vMil=Math.round((tm.getMilliseconds())/10); //arrondi au centième
+   if (vMin<10){
+      vMin="0"+vMin;
+   }
+   if (vSec<10){
+      vSec="0"+vSec;
+   }
+   if (vMil<10){
+      vMil="0"+vMil;
+   }
+   document.getElementById("divChrono").innerHTML=vMin+":"+vSec+":"+vMil;
 }
-function chronoStart(){
-	document.chronoForm.startstop.value = "stop!"
-	document.chronoForm.startstop.onclick = chronoStop
-	document.chronoForm.reset.onclick = chronoReset
-	start = new Date()
-	chrono()
+function fChrono(){
+   tmNow=new Date();
+   Interv=tmNow-tmStart;
+   tmInterv=new Date(Interv);
+   affTime(tmInterv);
 }
-function chronoContinue(){
-	document.chronoForm.startstop.value = "stop!"
-	document.chronoForm.startstop.onclick = chronoStop
-	document.chronoForm.reset.onclick = chronoReset
-	start = new Date()-diff
-	start = new Date(start)
-	chrono()
+function fStart(){
+   fStop();
+   if (tmInterv==0) {
+      tmStart=new Date();
+   } else { //si on repart après un clic sur Stop
+      tmNow=new Date();
+      Pause=tmNow-tmInterv;
+      tmStart=new Date(Pause);
+   }
+   setTm=setInterval(fChrono,10); //lancement du chrono tous les centièmes de secondes
 }
-function chronoReset(){
-	document.getElementById("chronotime").value = "0:00:00:000"
-	start = new Date()
+function fLap(){ //enregistrement des temps intermédiaires
+   tTime[nTime]=tmInterv;
+   nTime++;
 }
-function chronoStopReset(){
-	document.getElementById("chronotime").value = "0:00:00:000"
-	document.chronoForm.startstop.onclick = chronoStart
+function fStop(){
+   clearInterval(setTm);
+   tTime[nTime]=tmInterv;
 }
-function chronoStop(){
-	document.chronoForm.startstop.value = "start!"
-	document.chronoForm.startstop.onclick = chronoContinue
-	document.chronoForm.reset.onclick = chronoStopReset
-	clearTimeout(timerID)
+function fMemory(){ //affichage des temps intermédiaires à partir du dernier
+   if(nTime>0){
+      nTime--;
+   } else {
+      nTime=tTime.length-1;//on repart au dernier temps une fois arrivé au premier
+   }
+   affTime(tTime[nTime]);
+}
+function fReset(){ //on efface tout
+   fStop();
+   tmStart=0;
+   tmInterv=0;
+   tTime=[];
+   nTime=0;
+   document.getElementById("divChrono").innerHTML="00:00:00";
 }
