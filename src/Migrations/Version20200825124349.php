aa730872ea2ee95e38c1d52655b922574d110534
e-guyot <guyot.estellee@gmail.com>
2020-08-25 15:25:21 +0200

    fix timer

--- src/Migrations/Version20200825124349.php
+++ src/Migrations/Version20200825124349.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200825104405 extends AbstractMigration
+final class Version20200825124349 extends AbstractMigration
 {
     public function getDescription() : string
     {
